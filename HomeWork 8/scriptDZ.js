4b342af5f1d74d21f22bd7670094fc9736995ab0
DimaGitHubKoretski <karetskidima@gmail.com>
2022-11-19 23:47:47 +0100

    DZ 8

---
+++ HomeWork 8/scriptDZ.js
@@ -0,0 +1,116 @@
+//#1
+// let d = 7;
+// console.log(d);
+// console.log(d**2);
+// console.log(d**3);
+//#2
+// let num=Number(prompt('Enter the Namber'));
+// if(num>0){
+//     console.log(num*2)
+// }
+//#3
+// let num=Number(prompt('Enter the Namber'));
+// if(num<0){
+//     console.log('отрицательное');
+// } else if(num==0) {
+//     console.log('ноль');
+// } else if(num>0){
+//     console.log('положительное');
+// }
+//#4
+// let num1=Number(prompt('Enter the Namber'));
+// let num2=Number(prompt('Enter the Namber'));
+
+// if(num1>num2){
+//     let a=num1+num2;
+//     console.log(a);
+// }else if(num1<num2){
+//     let b=num1*num2;
+//     console.log(b);
+// }else if(num1==num2){
+//     console.log('числа одинаковые');
+// }
+//#5
+// let num=Number(prompt('Enter the Namber'));
+// if(num<0){
+//     let a=num**2;
+//     console.log(a);
+// }else if(num>=num){
+//     console.log('ошибка');
+// }
+//#6
+// let num1=Number(prompt('Enter the Namber'));
+// let num2=Number(prompt('Enter the Namber'));
+// if(num1<=num2){
+//     console.log('стульев хватает');
+// }else if(num1>num2){
+//     console.log('стульев не хватает');
+// }
+//#7
+// let arr =[1,2,3,4,5,6,7,8,9];
+// console.log('Старит '+ arr + ' финиш');
+//#8
+// for(let i =10; i<=99;i++){
+//     if(i%5==0){
+//         console.log(i);
+//     }
+// }
+//#9  подсмотрел в интернете как делать
+// let a = Number(prompt('Enter the Namber'));
+// if (a / 2 ) {
+// a = a - 1;
+// }
+// for (i = 1; i <= 7; i++) {
+// a = a + 2;
+// console.log(a);;
+// }
+//#10
+// let arr =[-5,-2,12,15,22,-24,25,-67,77,88,84,12];
+//     for(let i =0; i<11;i++){
+//       if(arr[i]>=15){
+//          console.log(arr[i]);
+//       }
+// }
+//#11
+// let arr =[-5,7,3,-2,12,15,22,-23,25,-67,77,81,84,12];
+//    for(let i =0; i<13;i++){
+//       if(arr[i]%2!==0){   //arr[i]%2 либо просто таки корое и тоже есть правельно
+//       console.log(arr[i]);
+//    }
+// }
+//#12
+// let arr =[-5,7,3,-2,12,15,22,-23,25,9,77,81,84,12];
+//    for(let i =0; i<13;i++){
+//       if(arr[i]%3==0){
+//       console.log(arr[i]);
+//    }
+// }
+//#13 не понял сути)
+
+
+
+// #14 
+// let arr =[-5,7,3,-2,12,15,22,-23,25,9,77,81,84,12];
+// let sum =0;
+// for(let i =0; i<arr.length;i++){
+//    if(arr[i]%2) 
+//    sum=sum+arr[i];{
+//    }
+// } 
+// console.log(sum)
+//#15 не без помощи интерента 
+// let arr = [1,2,3,4,5,6,7,8,9,10] 
+//         let sum = 0;
+//         let sum1 =0;
+//         let sum2=0;
+//         let sum3=0;
+//         for (let i=0; i < arr.length; i++){
+//             if (arr[i] % 2 == 0){
+//                 sum += arr[i];
+//             }if (arr[i] % 2){
+//                sum1 += arr[i];
+//         }if(sum1>sum){
+//          sum2=sum1-sum;
+//         }
+//       } 
+// console.log(sum2)
